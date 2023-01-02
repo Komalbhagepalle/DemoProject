03670d5769fad6690dddb2709b7bf96dd8e8b87b
kbhag <kbhag@LAPTOP-BSJKEKIV>
2023-01-02 13:55:06 +0530

    first commit...

---
+++ src/main/java/PraticeForSelenium/DemoProject/Demo.java
@@ -0,0 +1,15 @@
+package PraticeForSelenium.DemoProject;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("est something...");
+		else {
+			System.out.println("do work..");
+		}
+
+	}
+
+}
